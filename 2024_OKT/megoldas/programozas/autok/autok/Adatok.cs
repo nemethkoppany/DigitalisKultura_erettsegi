92cf4d8c94c793de0a61cec12bd21cf9fdf66dc8
nemethkoppany <koppany344@gmail.com>
2026-05-13 23:41:32 +0200

    24 oktober programozas majdnem kesz (hianyzik a 6. es 7. feladat)

---
+++ 2024_OKT/megoldas/programozas/autok/autok/Adatok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok
+{
+    internal class Adatok
+    {
+        public string Rendszam {  get; set; }
+        public int Ora {  get; set; }
+        public int Perc {  get; set; }
+        public int Sebesseg { get; set; }
+
+        public Adatok(string rendszam, int ora, int perc, int sebesseg)
+        {
+            Rendszam = rendszam;
+            Ora = ora;
+            Perc = perc;
+            Sebesseg = sebesseg;
+        }
+    }
+}
---
+++ 2024_OKT/megoldas/programozas/autok/autok/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace autok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            List<Adatok> adatok = new List<Adatok>();
+            string[] lines = File.ReadAllLines("jeladas.txt");
+
+            foreach (string line in lines)
+            {
+                string[] lineparts = line.Split("\t");
+                Adatok adat = new Adatok(
+                        rendszam: lineparts[0],
+                        ora: int.Parse(lineparts[1]),
+                        perc: int.Parse(lineparts[2]),
+                        sebesseg: int.Parse(lineparts[3])
+                );
+                adatok.Add(adat);
+            }
+
+
+            Console.WriteLine("2. feladat: ");
+            var utolso = adatok.Last();
+
+            Console.WriteLine($"Az utolsó jeladás időpontja {utolso.Ora}:{utolso.Perc}, a jármű rendszáma {utolso.Rendszam}");
+
+            Console.WriteLine("3. feladat: ");
+            var elso = adatok.First();
+
+
+            Console.WriteLine($"Az első jármű: {elso.Rendszam}");
+            Console.Write($"Jeladásainak időpontja: ");
+            foreach(var e in adatok)
+            {
+                if(e.Rendszam == elso.Rendszam)
+                {
+                    Console.Write($" {e.Ora}:{e.Perc} ");
+                }
+            }
+
+
+            Console.WriteLine("4. feladat:");
+            Console.Write("Kérem, adja meg az órát: ");
+            int bekert_ora = int.Parse( Console.ReadLine());
+            Console.Write("Kérem, adja meg a percet: ");
+            int bekert_perc = int.Parse( Console.ReadLine());
+
+            int jeladasok = 0;
+
+            foreach(var e in adatok)
+            {
+                if(e.Ora == bekert_ora && e.Perc == bekert_perc)
+                {
+                    jeladasok++;
+                }
+            }
+
+            Console.WriteLine($"Jeladások száma: {jeladasok}");
+
+            Console.WriteLine("5. feladat:");
+            var leggyorsabb = adatok.Max(x => x.Sebesseg);
+            Console.WriteLine($"A legnagyobb sebesség km/h: {leggyorsabb}");
+            Console.Write($"A járművek: ");
+            foreach (var e in adatok)
+            {
+                if(e.Sebesseg == leggyorsabb)
+                {
+                    Console.Write($"{e.Rendszam} ");
+                }
+            }
+
+
+        }
+    }
+}
